f59c2d2b46a3b8a065554f330062e22e6ce52974
Jorge Baez <jibm_82@yahoo.com.mx>
2019-01-29 20:42:06 -0600

    Implemented environment variables for mysql config

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // Require Modules
+require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
-const SequelizeStore = require('connect-session-sequelize')(session.Store); // <---------- Mongo Session Store
+const SequelizeStore = require("connect-session-sequelize")(session.Store); // <---------- Mongo Session Store
 // Passport and Passport Strategy Required
 const routes = require("./routes");
-const passport = require('./passport'); // <- Passport configuration has been done in ./passport
+const passport = require("./passport"); // <- Passport configuration has been done in ./passport
 
 // Create the Server/App Object
 const app = express();
@@ -17,21 +18,23 @@ const db = require("./models");
 
 // *********  MIDDLEWARE SET UP SECTION  *************
 // Set Up URL encoded to allow understanding of body's Data coming from Form POST
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 // Set Up URL encoded to allow understanding of body's Data coming from json POST
 app.use(express.json());
 // Set Up the public directory that will serve the static files under /.
 app.use(express.static("public"));
 // Set Up the Session Middleware require("express-session")
 // https://github.com/expressjs/session
-app.use(session({
-  store: new SequelizeStore({
+app.use(
+  session({
+    store: new SequelizeStore({
       db: db.sequelize
-  }), // <---------- Session Store
-  secret: "shhh this is super super secret", // This is the secret used to sign the session ID cookie.
-  resave: false,
-  saveUninitialized: true
-}))
+    }), // <---------- Session Store
+    secret: "shhh this is super super secret", // This is the secret used to sign the session ID cookie.
+    resave: false,
+    saveUninitialized: true
+  })
+);
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,18 +48,19 @@ if (process.env.NODE_ENV === "production") {
 // *********  ROUTES  *************
 app.use(routes);
 
-
 var syncOptions = {
   force: true
 };
 
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
-};
+}
 
 // Initialize the Server
-db.sequelize.sync(syncOptions).then(function () {
-  app.listen(PORT, () => (
-    console.log(`Server Listening in Port: ${PORT}, http://localhost:${PORT} 🌎`)
-  ));
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, () =>
+    console.log(
+      `Server Listening in Port: ${PORT}, http://localhost:${PORT} 🌎`
+    )
+  );
 });
