5b967be07f173ee0d005b9ca97c62146dae6a487
Seiji Osada <seiji13r@outlook.com>
2019-02-01 12:27:18 -0600

    Server Sessions Expiration in Store Updated

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ app.use(express.static("public"));
 app.use(
   session({
     store: new SequelizeStore({
-      db: db.sequelize
+      db: db.sequelize,
+      checkExpirationInterval: 3 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+      expiration: 2 * 24 * 60 * 60 * 1000  // The maximum age (in milliseconds) of a valid session.
     }), // <---------- Session Store
     secret: "shhh this is super super secret", // This is the secret used to sign the session ID cookie.
     resave: false,
