56353270d962eb092bcac18b77a4f747682bffef
Seiji Osada <seiji13r@outlook.com>
2019-02-06 06:48:40 -0600

    Remove unnecesary comments and console log in Authentication Module

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -6,18 +6,6 @@ const strategy = new LocalStrategy(
     usernameField: 'email' // not necessary, DEFAULT
   },
   (email, password, done) => {
-    // User.findOne({ email }, (err, user) => {
-    // 	if (err) {
-    // 		return done(err)
-    // 	}
-    // 	if (!user) {
-    // 		return done(null, false, { message: 'Incorrect username' })
-    // 	}
-    // 	if (!user.checkPassword(password)) {
-    // 		return done(null, false, { message: 'Incorrect password' })
-    // 	}
-    // 	return done(null, user)
-    // })
     db.User.findOne({
       where: {
         email: email
