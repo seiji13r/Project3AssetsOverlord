e1810b1a49072b8107bde0683534fc173231c0e1
Jorge Baez <jibm_82@yahoo.com.mx>
2019-02-15 21:26:22 -0600

    Updated dashboard layout

--- client/src/routes/dashboard.jsx
+++ client/src/routes/dashboard.jsx
@@ -6,12 +6,6 @@ import People from "@material-ui/icons/People";
 import SettingsInputAntenna from "@material-ui/icons/SettingsInputAntenna";
 import Home from "@material-ui/icons/Home";
 
-// import ContentPaste from "@material-ui/icons/ContentPaste";
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Notifications from "@material-ui/icons/Notifications";
-import Unarchive from "@material-ui/icons/Unarchive";
 // core components/views
 import DashboardPage from "views/Dashboard/Dashboard.jsx";
 
@@ -23,15 +17,9 @@ import AuthLogIn from "views/AuthLogIn";
 import AuthSignUp from "views/AuthSignUp";
 
 import UserProfile from "views/UserProfile/UserProfile.jsx";
-import TableList from "views/TableList/TableList.jsx";
-import Typography from "views/Typography/Typography.jsx";
-import Icons from "views/Icons/Icons.jsx";
-import Maps from "views/Maps/Maps.jsx";
-import NotificationsPage from "views/Notifications/Notifications.jsx";
 import LandingPage from "../views/LandingPage";
 
 const dashboardRoutes = [
- 
   {
     path: "/welcome",
     sidebarName: "Welcome",
@@ -82,49 +70,13 @@ const dashboardRoutes = [
     component: UsersPage
   },
   {
-    path: "/user",
+    path: "/profile",
     sidebarName: "Profile",
     navbarName: "Profile",
     icon: Person,
     component: UserProfile
   },
   { redirect: true, path: "/", to: "/welcome", navbarName: "Redirect" }
-  // {
-  //   path: "/table",
-  //   sidebarName: "Table List",
-  //   navbarName: "Table List",
-  //   icon: "content_paste",
-  //   component: TableList
-  // },
-  // {
-  //   path: "/typography",
-  //   sidebarName: "Typography",
-  //   navbarName: "Typography",
-  //   icon: LibraryBooks,
-  //   component: Typography
-  // },
-  // {
-  //   path: "/icons",
-  //   sidebarName: "Icons",
-  //   navbarName: "Icons",
-  //   icon: BubbleChart,
-  //   component: Icons
-  // },
-  // {
-  //   path: "/maps",
-  //   sidebarName: "Maps",
-  //   navbarName: "Map",
-  //   icon: LocationOn,
-  //   component: Maps
-  // },
-  // {
-  //   path: "/notifications",
-  //   sidebarName: "Notifications",
-  //   navbarName: "Notifications",
-  //   icon: Notifications,
-  //   component: NotificationsPage
-  // },
-
 ];
 
 export default dashboardRoutes;
